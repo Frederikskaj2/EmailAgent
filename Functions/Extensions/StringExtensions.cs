255dafd85d31b084934f91bdd0a075c28d8f749c
Martin Liversage <martin@liversage.com>
2020-07-05 14:38:07 +0200

    Improve escaping of markdown

--- Functions/Extensions/StringExtensions.cs
+++ Functions/Extensions/StringExtensions.cs
@@ -5,9 +5,9 @@ namespace Frederikskaj2.EmailAgent.Functions.Extensions
 {
     internal static class StringExtensions
     {
-        private static readonly Regex markdownRegex = new Regex(@"[\\`\*_\{\}\[\]\(\)<>#\+\-\.!\|]");
+        private static readonly Regex markdownRegex = new Regex(@"[\\`\*_\{\}\[\]\(\)#\+\-\.!]");
 
-        public static string EscapeMarkdown(this string @string) => markdownRegex.Replace(@string, match => @"\" + match.Captures[0].Value);
+        public static string EscapeMarkdown(this string @string) => markdownRegex.Replace(@string, match => $"&#{(int) match.Captures[0].Value[0]};");
 
         public static string EscapeJsonString(this string @string) => JsonConvert.ToString(@string)[1..^1];
     }
